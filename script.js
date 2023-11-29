05ed3343c520d64140bd5076562acf97be011f90
Damon Haase <damon.haase@gmail.com>
2023-11-29 12:43:33 -0500

    Fix tracking turn. Fix deterining winner

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gameBoard = (function () {
         board.push("empty");
     }
     //#region Arrays of rows/cols/diagonals
-    let topRow = [board[0-2]];
+    let topRow = [board[0], board[1], board[2]];
     let midRow = [board[3], board[4], board[5]];
     let botRow = [board[6], board[7], board[8]];
 
@@ -15,7 +15,7 @@ const gameBoard = (function () {
     let diag1 = [board[0], board[4], board[8]];
     let diag2 = [board[2], board[4], board[6]];
 //#endregion
-    const potentialThreesInARow = [topRow, midRow, botRow, leftCol, midCol, rightCol, diag1, diag2];
+    
 
     const updateBoard = () => {
         topRow = [board[0], board[1], board[2]];
@@ -37,32 +37,39 @@ const gameBoard = (function () {
     }
 
     const endTurn = (player) => {
-        if (player === player1) player2.isTurn = true;
-        else player1.isTurn = true;
+        if (player === player1) {
+            player2.isTurn = true;
+            player1.isTurn = false;
+        }
+        else if (player === player2) {
+            player1.isTurn = true;
+            player2.isTurn = false;
+        }
     }
 
     const changeBoard = (player, index) => {
         if (player.isTurn) {
             if (board[index] == "empty") {
                     board[index] = player.symbol;
-                    endTurn(player);
                     updateBoard();
                     displayBoard();
+                    checkWin(player);
+                    endTurn(player);
             }
+            else console.log('That space is already taken!');
         }
         else console.log(`It isn't your turn yet, ${player.name}!`);
     }
 
-    const checkWin = () => {
+    const checkWin = (player) => {
+        //get all possible rows
+        const potentialThreesInARow = [topRow, midRow, botRow, leftCol, midCol, rightCol, diag1, diag2];
         //for each possible winning row...
         potentialThreesInARow.forEach(row => {
             //check each cell of that row
-            if (row.every (ele => {
-                
-                //look into how to use Array.every
-                //goal is it should cleanly evaluate true or false based on whether or not it's all X's or all O's
-            }));
+            if (row.every(ele => ele == player.symbol)) console.log(`${player.name} wins!`);
         });
+        
     }
 
     return { changeBoard}
@@ -72,19 +79,17 @@ const displayController = (function () {
     
 })();
 
-//add parameter to choose X or O?
 function player (name, symbol, isWinner = false, isTurn = false) {
     return { name, symbol, isWinner, isTurn};
 } 
 
-const player1 = player("Bob", "X");
-const player2 = player("NPC", "O");
+const player1 = player("John", "X");
+const player2 = player("Jane", "O");
 
 player1.isTurn = true;
 console.log(player1);
 console.log(player2);
 gameBoard.changeBoard(player1, 0);
-gameBoard.changeBoard(player2, 5);
+gameBoard.changeBoard(player2, 0);
 gameBoard.changeBoard(player1, 2);
 gameBoard.changeBoard(player2, 6);
-gameBoard.changeBoard(player1, 1);
